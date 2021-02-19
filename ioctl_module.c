384858244ccb4c1aa4ab14fd08e9feab014003a8
AeonSw4n <piotrek.nojszewski@gmail.com>
2021-02-19 05:48:15 -0500

    final

--- ioctl_module.c
+++ ioctl_module.c
@@ -1,71 +1,205 @@
-/*
- *  ioctl test module -- Rich West.
- */
-
+#include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/init.h>
+#include <linux/sched.h>
+#include <linux/workqueue.h>
+#include <linux/interrupt.h>
+//#include <asm/io.h>
+#include <linux/unistd.h>
 #include <linux/errno.h> /* error codes */
 #include <linux/proc_fs.h>
 #include <asm/uaccess.h>
 #include <linux/tty.h>
-#include <linux/sched.h>
+#include <linux/delay.h>
 
 MODULE_LICENSE("GPL");
 
-/* attribute structures */
-struct ioctl_test_t {
-  int field1;
-  char field2;
+static struct workqueue_struct *my_workqueue;
+
+struct key_press {
+    char letter;
+    char sent;
+} key;
+
+struct nothing {
+    char imr;
 };
 
 #define IOCTL_TEST _IOW(0, 6, struct ioctl_test_t)
+#define KEYBOARD _IOR(1, 7, struct key_press)
+#define REGISTER _IOW(0, 8, struct nothing)
+#define DEREGISTER _IOW(0, 9, struct nothing)
 
 static int pseudo_device_ioctl(struct inode *inode, struct file *file,
-			       unsigned int cmd, unsigned long arg);
+                               unsigned int cmd, unsigned long arg);
 
 static struct file_operations pseudo_dev_proc_operations;
 
 static struct proc_dir_entry *proc_entry;
 
-static int __init initialization_routine(void) {
-  printk("<1> Loading module\n");
+// Inline assembly Functions
+static inline unsigned char inb( unsigned short usPort ) {
 
-  pseudo_dev_proc_operations.ioctl = pseudo_device_ioctl;
+    unsigned char uch;
 
-  /* Start create proc entry */
-  proc_entry = create_proc_entry("ioctl_test", 0444, NULL);
-  if(!proc_entry)
-  {
-    printk("<1> Error creating /proc entry.\n");
-    return 1;
-  }
-
-  //proc_entry->owner = THIS_MODULE; <-- This is now deprecated
-  proc_entry->proc_fops = &pseudo_dev_proc_operations;
-
-  return 0;
+    asm volatile( "inb %1,%0" : "=a" (uch) : "Nd" (usPort) );
+    return uch;
 }
 
+static inline void outb( unsigned char uch, unsigned short usPort ) {
+
+    asm volatile( "outb %0,%1" : : "a" (uch), "Nd" (usPort) );
+}
+
+static char character;
+static char cc;
+static char enabled;
+
+static char key_pressed(){
+    //1 released
+    //0 pressed
+    return *((char *)inb(0x60)) & 0x80 ? 1: 0;
+}
+
+// Character retrieval method
+char my_getchar ( void ) {
+
+    char c;
+    unsigned char status;
+    static char scancode[128] = "\0\e1234567890-=\177\tqwertyuiop[]\n\0asdfghjkl;'`\0\\zxcvbnm,./\0*\0 \0\0\0\0\0\0\0\0\0\0\0\0\000789-456+1230.\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
+
+
+    /* Poll keyboard status register at port 0x64 checking bit 0 to see if
+     * output buffer is full. We continue to poll if the msb of port 0x60
+     * (data port) is set, as this indicates out-of-band data or a release
+     * keystroke
+     */
+    //while( !(inb( 0x64 ) & 0x1) || ( ( c = inb( 0x60 ) ) & 0x80 ) );
+    //printk("1");
+    if(!( ( c = inb( 0x60 ) ) & 0x80 )){
+        //printk("2");
+        c = inb(0x60);
+        return scancode[ (int)c ];
+    }
+    else {
+        return 0;
+    }
+    //status = inb(0x64);
+    //c = inb(0x60);
+    //printk("%c",scancode[ (int)c ]);
+    //static char s[1];
+    //s[0] = scancode[(int)c];
+    //char *sptr = &s;
+    //*(sptr++) = (char)0;
+    //my_printk(s);
+    //return scancode[ (int)c ];
+
+}
+
+char toUpper (char c, char keyPress){
+    if(keyPress){
+        if(c >= 'a' && c<='z')
+            return (char)(c-22);
+        switch(c){
+            case '0':
+                return ')';
+            case '1':
+                return '!';
+            case '2':
+                return '@';
+            case '3':
+                return '#';
+            case '4':
+                return '$';
+            case '5':
+                return '%';
+            case '6':
+                return '^';
+            case '7':
+                return '&';
+            case '8':
+                return '*';
+            case '9':
+                return '(';
+        }
+    }
+    return c;
+}
 /* 'printk' version that prints to active tty. */
 void my_printk(char *string)
 {
-  struct tty_struct *my_tty;
+    struct tty_struct *my_tty;
 
-  my_tty = current->signal->tty;
+    my_tty = current->signal->tty;
+    //printk("%s %d\n", string, strlen(string));
+    if (my_tty != NULL) {
+        (*my_tty->driver->ops->write)(my_tty, string, strlen(string));
+        (*my_tty->driver->ops->write)(my_tty, "\015\012", 2);
+    }
+}
 
-  if (my_tty != NULL) {
-    (*my_tty->driver->ops->write)(my_tty, string, strlen(string));
-    (*my_tty->driver->ops->write)(my_tty, "\015\012", 2);
-  }
-} 
+irqreturn_t irq_handler(int irq, void *dev_id, struct pt_regs *regs){
+    static int init = 0;
+    //static unsigned char scancode;
+    //static struct work_struct task;
+    unsigned char status;
 
 
+//    status = inb(0x64);
+//    scancode = inb(0x60);
+
+    if (init == 0){
+        printk("init");
+        //INIT_WORK(&task, got_char);
+        init = 1;
+    } else {
+        //printk("got something");
+        char c = my_getchar();
+        if(c){
+            key.letter = c;
+            character = c;
+            //char str[1];
+            //str[0] = c;
+            //my_printk(str);
+            enabled = 1;
+
+            //cc = toUpper(c, key_pressed());
+            //printk("3");
+        }
+        //PREPARE_WORK(&task, got_char, &scancode);
+    }
+    //queue_work(my_workqueue, &task);
+    return IRQ_HANDLED;
+}
+
+// Init Routine
+static int __init initialization_routine(void) {
+    printk("<1> Loading module\n");
+    pseudo_dev_proc_operations.ioctl = pseudo_device_ioctl;
+    /* Start create proc entry */
+    proc_entry = create_proc_entry("ioctl_test", 0444, NULL);
+    if(!proc_entry)
+    {
+        printk("<1> Error creating /proc entry.\n");
+        return 1;
+    }
+    //free_irq(1, NULL);
+    character = -1;
+    enabled = 0;
+    msleep(1000);
+    proc_entry->proc_fops = &pseudo_dev_proc_operations;
+    //my_workqueue = create_workqueue(MY_WORK_QUEUE_NAME);
+    my_workqueue = create_singlethread_workqueue("0BrainCellsQueue");
+    free_irq(1, (void *)(irq_handler));
+    return request_irq(1, irq_handler, IRQF_SHARED, "ioctl_test", (void *)(irq_handler));
+    //return 0;
+}
+
+// Clean up routine
 static void __exit cleanup_routine(void) {
-
-  printk("<1> Dumping module\n");
-  remove_proc_entry("ioctl_test", NULL);
-
-  return;
+    printk("<1> Dumping module\n");
+    free_irq(1, (void *)(irq_handler));
+    remove_proc_entry("ioctl_test", NULL);
+    return;
 }
 
 
@@ -73,28 +207,45 @@ static void __exit cleanup_routine(void) {
  * ioctl() entry point...
  */
 static int pseudo_device_ioctl(struct inode *inode, struct file *file,
-				unsigned int cmd, unsigned long arg)
+                               unsigned int cmd, unsigned long arg)
 {
-  struct ioctl_test_t ioc;
-  
-  switch (cmd){
+    struct key_press key;
 
-  case IOCTL_TEST:
-    copy_from_user(&ioc, (struct ioctl_test_t *)arg, 
-		   sizeof(struct ioctl_test_t));
-    printk("<1> ioctl: call to IOCTL_TEST (%d,%c)!\n", 
-	   ioc.field1, ioc.field2);
+    switch (cmd){
+        case REGISTER:
+            //sleep(0.10);
+            //disable_irq(1);
+            //sleep(0.10);
+            printk("<1>Disabled IRQ\n");
+            break;
+        case DEREGISTER:
+            //sleep(0.10);
+            //enable_irq(1);
+            printk("<1>Enabled IRQ\n");
+            break;
+        case KEYBOARD:
+            //key.letter ='a';
+            //my_printk("Disabled IRQ\n");
+            //key.letter = my_getchar();
+            //my_printk("Got Char\n");
+            //ioctl_arg = arg;
+            if(enabled){
+                key.letter = character;
+                key.sent = 1;
+                enabled = 0;
+                copy_to_user((struct key_press *)arg, &key, sizeof(struct key_press));
+            }
 
-    my_printk ("Got msg in kernel\n");
-    break;
-  
-  default:
-    return -EINVAL;
-    break;
-  }
-  
-  return 0;
+            break;
+
+        default:
+            return -EINVAL;
+            break;
+    }
+
+    return 0;
 }
 
-module_init(initialization_routine); 
-module_exit(cleanup_routine); 
+// Init and Exit declaration
+module_init(initialization_routine);
+module_exit(cleanup_routine);
--- ioctl_test.c
+++ ioctl_test.c
@@ -1,23 +1,85 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
+#include <unistd.h>
 
 #define IOCTL_TEST _IOW(0, 6, struct ioctl_test_t)
+#define KEYBOARD _IOR(1, 7, struct key_press)
+#define REGISTER _IOW(0, 8, struct nothing)
+#define DEREGISTER _IOW(0, 9, struct nothing)
+
+// Used to get a char from the keyboard using IOCTL
+struct key_press {
+    char letter;
+    char sent;
+};
+
+struct nothing {
+    char imr;
+};
+
+void my_getchar(int fd, struct key_press key);
 
 int main () {
 
-  /* attribute structures */
-  struct ioctl_test_t {
-    int field1;
-    char field2;
-  } ioctl_test;
+    /* IOCTL_TEST STUFF
+    // attribute structures
+    struct ioctl_test_t {
+      int field1;
+      char field2;
+    } ioctl_test;
 
-  int fd = open ("/proc/ioctl_test", O_RDONLY);
-  printf("%d\n", fd);
-  ioctl_test.field1 = 10;
-  ioctl_test.field2 = 'a';
+    ioctl_test.field1 = 10;
+    ioctl_test.field2 = 'a';
 
-  ioctl (fd, IOCTL_TEST, &ioctl_test);
+    ioctl (fd, IOCTL_TEST, &ioctl_test);
+    */
+    struct key_press key;
+    key.sent = 0;
+    int fd = open ("/proc/ioctl_test", O_RDONLY);
+    struct nothing MegaRetard;
+    sleep(1);
+    //ioctl(fd, REGISTER, &MegaRetard);
+    /* This loops until the system detects that the user has pressed the enter key */
+    int done = 0;
+    char letter;
+    while (!done) {
+        /* the character is both in key and letter (debugging stuff I dealt with) */
+        my_getchar(fd, key);
+        if(key.sent){
+            key.sent = 0;
+            letter = key.letter;
+            /*
+            if (letter == '\n') {
+                printf("here??\n");
+                //printf("\nEntered if statement\n");
+                done = 1;
+            }*/
+            //printf("%c\n",letter);
 
-  return 0;
+            if ((int)letter == 127){
+                char buff[] = "\b \b";
+                write(1, buff, sizeof(buff)-1);
+            } else {
+                char s[3];
+                s[0]=letter;
+                s[1]=' ';
+                s[2]='\b';
+                write(1, s, 3);
+            }
+        }
+        //char empty[] = " \b";
+        //write(1, empty, 2);
+        //printf("(%d-%c)\n", (int)letter, letter);
+        //fflush(stdin);
+    }
+    //printf("Exiting");
+    //fflush(stdin);
+    //ioctl(fd, DEREGISTER, &MegaRetard);
+    return 0;
+}
+
+void my_getchar(int fd, struct key_press key) {
+    ioctl (fd, KEYBOARD, &key);
+    //return key;
 }
